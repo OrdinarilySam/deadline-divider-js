81226c4bd87f6d14e9679da39dc053f4c437e7c8
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-03-19 12:47:30 -0500

    Added datefield

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import FieldBox from './components/FieldBox'
+import DateField from './components/DateField'
 import './App.css'
 
 function App() {
 
   const [values, setValues] = useState([])
+  const [dates, setDates] = useState([])
 
   return (
     <div>
       <h1>Placeholder</h1>
+      <DateField
+        dates={dates}
+        setDates={setDates}
+      />
       <FieldBox 
         setValues={setValues}
       />
---
+++ src/components/DateField.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function DateField(){
+    return (
+        <div>
+            <input type="date" />
+            <input type="date" />
+        </div>
+    )
+}
